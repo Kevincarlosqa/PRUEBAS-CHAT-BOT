6d15b88c7612d4e76848075058ce79b18944c0bd
CarlosCo_Dev <condorpasa26@gmail.com>
2025-02-20 21:50:41 -0500

    recibiendo json

--- src/pages/api/telegram/chatbot.ts
+++ src/pages/api/telegram/chatbot.ts
@@ -16,39 +16,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log('recibiendo algo')
   
-  // if (req.method === 'POST') {
     const { message } = req.body;
+    const chatId = message.chat.id;
+    const text = message.text;
 
-      const chatId = message.chat.id;
-      const text = message.text;
-
-      // const data = callback_query.data
-      // console.log(text)
-      // Aquí puedes manejar el mensaje de los usuarios
-      try{
-        await axios.post(genHTTP('sendMessage'),{
-          chat_id:chatId,
-          text: `Elije una opcion [${JSON.stringify(req.body)}]`,
-          "reply_markup": {
-            "inline_keyboard": [
-              [
-                { "text": "Opción 1", "callback_data": "eres cabro" },
-                { "text": "Opción 2", "callback_data": "el layme es cabro" }
-              ]
-            ]
-          }
-        })
-        res.status(200).json({message: 'mensaje enviado correctamente'})
-      }catch(err){
-        console.log(err)
-      }
-      // res.status(200).json({ message: 'todo ok' });
-
-  // } else {
-  //   res.status(405).json({ error: 'Method Not Allowed' });
-  // }
+    try{
+      await axios.post(genHTTP('sendMessage'),{
+        chat_id:chatId,
+        text: JSON.stringify(req.body)
+      })
+      res.status(200).json({message: 'mensaje enviado correctamente'})
+    }catch(err){
+      console.log(err)
+    }
 }
 
 async function sendTelegramMessage(chatId: number, text: string) {
