afcf4c8def2d30a2038b5de50f4fac74bb72ccc3
CarlosCo_Dev <condorpasa26@gmail.com>
2025-02-22 17:48:38 -0500

    probando fluido

--- src/pages/api/telegram/chatbot.ts
+++ src/pages/api/telegram/chatbot.ts
@@ -2,6 +2,15 @@ import { genHTTP } from '@/helpers/telegram/helpers';
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+type TelegramMessage = {
+  message: {
+    chat: {
+      id: number;
+    };
+    text: string;
+  };
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -11,12 +20,28 @@ export default async function handler(
 
 
     try{
-      await axios.post(genHTTP('sendMessage'),{
-        chat_id: 1568853312,
-        text: 'Tu tienes un mensajito' + ' ' + JSON.stringify(body,null,2)
-      })
+      // await axios.post(genHTTP('sendMessage'),{
+      //   chat_id: body.message.chat.id,
+      //   text: 'Tu tienes un mensajito' + ' ' + JSON.stringify(body,null,2)
+      // })
       res.status(200).json({message: 'mensaje enviado correctamente'})
     }catch(err){
       console.log(err)
     }
 }
+
+// async function sendTelegramMessage(chatId: number, text: string) {
+//   const token = process.env.TELEGRAM_KEY;
+//   const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+//   await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       chat_id: chatId,
+//       text: text,
+//     }),
+//   });
+// }
