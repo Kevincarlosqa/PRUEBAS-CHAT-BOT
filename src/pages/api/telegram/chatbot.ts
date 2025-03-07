1c47f5c70387df8f4771dbe019dea4959d8afade
CarlosCo_Dev <condorpasa26@gmail.com>
2025-03-06 21:16:14 -0500

    ahora si porfa

--- src/pages/api/telegram/chatbot.ts
+++ src/pages/api/telegram/chatbot.ts
@@ -1,7 +1,7 @@
 // import { saveUserInfo, searchUser } from '@/helpers/json_routes';
 import { bookList, casesInfo, stageTwoOptions } from '@/helpers/hardInfo';
 import { Bot_BadOptionMessage, Bot_SendKeyboard, Bot_SendMessage, genHTTP } from '@/helpers/message';
-import { findUser } from '@/helpers/prisma';
+import { findUser, prisma } from '@/helpers/prisma';
 import { foo_stage00, foo_stage01, foo_stage02, foo_stage03, foo_stage04, foo_stage05, foo_stage06, foo_stage07, foo_stage08, foo_stage09, foo_stage10, foo_stage11 } from '@/helpers/stages';
 import { resUserMessage } from '@/helpers/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -32,18 +32,17 @@ export default async function handler(
     const {first_name,id,text} = getBodyInfo(body)
     try{
 
-      // const user = await findUser(id) //? Funcio o ruta para obtener el estado del usuario
-      // const user = await prisma.user.findFirst({where:{id}}) //? Funcio o ruta para obtener el estado del usuario
+      const user = await findUser(id) //? Funcio o ruta para obtener el estado del usuario
       
-      // if(user){
-      //   const {stage_id} = user
-      //   await foo_stages[stage_id](id,text)
-      // }else{
-      //   await foo_stage00(id,first_name)
-      // }
+      if(user){
+        const {stage_id} = user
+        await foo_stages[stage_id](id,text)
+      }else{
+        await foo_stage00(id,first_name)
+      }
       await Bot_SendMessage('funciona porfavor',id) // si pasa un error que envie nuestro telegram
 
-      return res.status(200).json({message:true ? 'chi' : 'nho'})
+      return res.status(200).json({message: `interaccion del usr ${id}`})
 
     }catch(err){
       try{
