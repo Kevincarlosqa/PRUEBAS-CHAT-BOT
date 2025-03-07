22c0666b8430273c55287d3816f2a37520e083ab
CarlosCo_Dev <condorpasa26@gmail.com>
2025-03-07 00:05:46 -0500

    ojala

--- src/pages/api/telegram/receive.ts
+++ src/pages/api/telegram/receive.ts
@@ -5,9 +5,22 @@ import { changeUserStatus, createUser, findUser, resetUser } from "@/helpers/pri
 import { Prisma } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { env } from "process";
 
 
-const prisma = new PrismaClient()
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+  });
+
+if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default async function handler(
   req: NextApiRequest,
