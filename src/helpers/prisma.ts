8cda2310e46492cb527ceb13fd9b1066d11adef3
CarlosCo_Dev <condorpasa26@gmail.com>
2025-03-07 00:30:19 -0500

    holi

--- src/helpers/prisma.ts
+++ src/helpers/prisma.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client"
 import { User_DB } from "./types"
-import { env } from "process";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -10,10 +9,10 @@ export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log:
-      env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
+    process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export const createUser = async (user_id:number) => {
   const data:User_DB = {
