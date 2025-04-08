be588025115f20224c04881e03f4d1f0a223f9f8
CarlosCo_Dev <condorpasa26@gmail.com>
2025-04-08 00:25:43 -0500

    agregando mas tablas

---
+++ prisma/migrations/20250408052317_agregando_tablas_2/migration.sql
@@ -0,0 +1,93 @@
+-- CreateTable
+CREATE TABLE "Biblio" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "autor" TEXT NOT NULL,
+    "type_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Biblio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "TypeBiblio" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "TypeBiblio_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Embedding" (
+    "id" SERIAL NOT NULL,
+    "biblio_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Embedding_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Case" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "ante" TEXT NOT NULL,
+    "exam" TEXT NOT NULL,
+
+    CONSTRAINT "Case_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "tipo" INTEGER NOT NULL,
+    "url" TEXT NOT NULL,
+    "case_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tema" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+
+    CONSTRAINT "Tema_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CasesOnTemas" (
+    "case_id" INTEGER NOT NULL,
+    "tema_id" INTEGER NOT NULL,
+
+    CONSTRAINT "CasesOnTemas_pkey" PRIMARY KEY ("case_id","tema_id")
+);
+
+-- CreateTable
+CREATE TABLE "BibliosOnTemas" (
+    "biblio_id" INTEGER NOT NULL,
+    "tema_id" INTEGER NOT NULL,
+
+    CONSTRAINT "BibliosOnTemas_pkey" PRIMARY KEY ("biblio_id","tema_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Biblio_title_key" ON "Biblio"("title");
+
+-- AddForeignKey
+ALTER TABLE "Biblio" ADD CONSTRAINT "Biblio_type_id_fkey" FOREIGN KEY ("type_id") REFERENCES "TypeBiblio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Embedding" ADD CONSTRAINT "Embedding_biblio_id_fkey" FOREIGN KEY ("biblio_id") REFERENCES "Biblio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD CONSTRAINT "Image_case_id_fkey" FOREIGN KEY ("case_id") REFERENCES "Case"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CasesOnTemas" ADD CONSTRAINT "CasesOnTemas_case_id_fkey" FOREIGN KEY ("case_id") REFERENCES "Case"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CasesOnTemas" ADD CONSTRAINT "CasesOnTemas_tema_id_fkey" FOREIGN KEY ("tema_id") REFERENCES "Tema"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BibliosOnTemas" ADD CONSTRAINT "BibliosOnTemas_biblio_id_fkey" FOREIGN KEY ("biblio_id") REFERENCES "Biblio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BibliosOnTemas" ADD CONSTRAINT "BibliosOnTemas_tema_id_fkey" FOREIGN KEY ("tema_id") REFERENCES "Tema"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
