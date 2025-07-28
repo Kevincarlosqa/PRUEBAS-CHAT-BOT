e4a808242a504119bb8fab6d0a5745cc3ed897df
CarlosCo_Dev <condorpasa26@gmail.com>
2025-07-28 01:45:08 -0500

    tmr

--- src/helpers/api/response.ts
+++ src/helpers/api/response.ts
@@ -4,4 +4,6 @@ import { NextResponse } from "next/server"
 
 export const goodResponse = (message:string,data?:Object) => NextResponse.json({message,data},{status:200})
 
-export const badResponse = (err:Object) => NextResponse.json(err,{status:400})
+export const badResponse = (err:Object) => {
+  return NextResponse.json(err,{status:400})
+}
